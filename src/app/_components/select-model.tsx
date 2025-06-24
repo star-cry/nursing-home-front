1abc3ce249a7f51ed5808c14aca1429d587b0df0
LEE DONG GYU <tenenger7125@gmail.com>
2025-06-25 02:39:08 +0900

    💄 모델 선택 페이지에서 최대한 스크롤 생기지 않도록 스타일 수정

--- src/app/_components/select-model.tsx
+++ src/app/_components/select-model.tsx
@@ -5,12 +5,12 @@ import { Carrot170, InvertCarrot170 } from '@/components/icon';
 import SelectModelCard from './select-model-card';
 
 const SelectModel = () => (
-  <div className="py-10 px-7.5 flex flex-col justify-between gap-30">
-    <div className="typo-bold-38">
+  <div className="py-10 px-7.5 flex flex-col h-full">
+    <div className="typo-bold-38 flex-1 max-h-[150px]">
       <div>케어플러스에</div>
       <div>오신것을 환영합니다!</div>
     </div>
-    <div className="flex flex-col gap-25">
+    <div className="flex flex-col justify-between max-h-[550px] h-full mb-auto flex-1">
       <SelectModelCard
         model="보호자"
         modelIcon={<Carrot170 />}
@@ -22,7 +22,7 @@ const SelectModel = () => (
         model="간병인"
         modelIcon={<InvertCarrot170 />}
         modelTextClassName="text-orange"
-        title="간병인을\등록하고 싶다면"
+        title="간병인을\n등록하고 싶다면"
       />
     </div>
   </div>
